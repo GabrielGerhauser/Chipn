d572893efa478671fae7aa802d0b30020f5fc28e
GabrielGerhauser <84334180+GabrielGerhauser@users.noreply.github.com>
2021-08-02 11:17:30 -0400

    Got started.

---
+++ backend/Chipn/Chipn/ChipnDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Chipn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chipn
+{
+	public class ChipnDBContext : DbContext
+	{
+		private DbSet<Account> AccountSet{get;set;}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var connectionString = "Server=(localdb)\\mssqllocaldb;Database=ChipnDB;Trusted_Connection=True;";
+			optionsBuilder.UseSqlServer(connectionString).UseLazyLoadingProxies();
+			base.OnConfiguring(optionsBuilder);
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Account>().HasData(
+				new Account(){Id=1,UserName="ChipnAdministrator",ChipCount=long.MaxValue,Email="admin@chipn.gov",Password="NoneOfYourBusiness",Age=int.MaxValue},
+				new Account(){Id=2,UserName="ChipnAdministratorCarlos",ChipCount=long.MinValue,Email="carlos@chipn.gov",Password="NoneOfYourCarlos",Age=36},
+				new Account(){Id=3,UserName="ChipnAdministratorDavis",ChipCount=long.MinValue,Email="davis@chipn.gov",Password="NoneOfYourDavis",Age=27}
+			);
+		}
+	}
+}
---
+++ backend/Chipn/Chipn/Models/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chipn.Models
+{
+	public class Account
+	{
+		public int Id {get;set;}
+
+		//
+
+		public string UserName {get;set;}
+		public long ChipCount {get;set;}
+		public string Email {get;set;}
+		public string Password {get;set;}
+		public int Age {get;set;}
+
+		/*
+		public decimal Money {get;set;}
+		public string BitcoinAddress {get;set;}
+		public string DOB {get;set;}
+		public string Contract {get;set;}
+		public int BloodCount {get;set;}
+		public int BloodType {get;set;}
+		public string CreditCard {get;set;}
+		public string SSN {get;set;}
+		*/
+
+		//
+
+
+	}
+}
