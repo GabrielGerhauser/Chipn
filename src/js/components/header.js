97b52fd700c698190a29ec13e7480a0666238f46
GabrielGerhauser <84334180+GabrielGerhauser@users.noreply.github.com>
2021-08-03 14:06:03 -0400

    Nav bar + Craps

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -5,10 +5,18 @@ export default function header() {
     headertext.innerHTML = `
     <nav> 
         <ol>
-            <li id="slotNav" class= "gamenav">Slots</li>
-            <li id="blackjackNav" class= "gamenav">Blackjack</li>
-            <li id="rouletteNav" class= "gamenav">High Stakes Roulette</li>
-            <li id="crapsNav" class= "gamenav">CRUD Craps 💩</li>
+            <li id="slotsNav" class= "gamenav">
+                <a href="games/slots/slots.html">Slots</a>
+            </li>
+            <li id="blackjackNav" class= "gamenav">
+                <a href="games/blackjack/blackjack.html">Blackjack</a>
+            </li>
+            <li id="rouletteNav" class= "gamenav">
+                <a href="games/roulette/roulette.html">High Stakes Roulette</a>
+            </li>
+            <li id="crapsNav" class= "gamenav">
+                <a href="games/craps/craps.html">CRUD Craps 💩</a>
+            </li>
         </ol>
     </nav>
     `;
