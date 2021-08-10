3b6eaa59696136bffed8d9d08ff7108dbda687be
JSLongwell <justin.longwell@gmail.com>
2021-08-10 10:58:02 -0400

    nothing

--- src/js/components/account.js
+++ src/js/components/account.js
@@ -4,37 +4,41 @@ import * as FETCH from "../api/actions";
 let userAccount = { Id : "1", UserName : "ChipnAdministrator", ChipCount : "5000", Email : "admin@chipn.gov", Password : "NoneOfYourBusiness", Age : "21" };
 // let userId = userAccount.Id;
 
-export default function account() {
+export function createAccountDiv() {
     let accountDiv = document.createElement("div");
     accountDiv.id = "account";
     accountDiv.style.float = "right";
-
-    // const fetchedAccount = FETCH.getAccount(userAccount.Id).then((result) => {
-    //     console.log(result);
-    // });
-
-    if(userAccount == null)
-    {
-        accountDiv.innerHTML = `
-            <button id="account-login">Log In</button>
-            <button id="account-signup">Sign Up</button>
-        `;
-        return accountDiv;
-    } else
-    {
-        FETCH.getAccount(userAccount.Id, data => {
-            console.log(data.Password);
-            if(data.Password == userAccount.Password) {
-                console.log("Success");
-            }
-        });
-
-
-        accountDiv.innerHTML =`
-        Name: ${userAccount.UserName}<br />
-        Chip Count: ${userAccount.ChipCount}
-        `;
-        return accountDiv;
-    }
+    accountDiv.innerHTML = " ";
+    return accountDiv;
 }
+
+// export function populateAccountDiv() {
+//     // const fetchedAccount = FETCH.getAccount(userAccount.Id).then((result) => {
+//     //     console.log(result);
+//     // });
+
+//     if(userAccount == null)
+//     {
+//         accountDiv.innerHTML = `
+//             <button id="account-login">Log In</button>
+//             <button id="account-signup">Sign Up</button>
+//         `;
+//         return accountDiv;
+//     } else
+//     {
+//         FETCH.getAccount(userAccount.Id, data => {
+//             // console.log(data);
+//             if(data.password == userAccount.Password) {
+//                 console.log("Success");
+//                 accountDiv.innerHTML =`
+//                 Name: ${userAccount.UserName}<br />
+//                 Chip Count: ${userAccount.ChipCount}
+//                 `;
+//                 return accountDiv;
+//             } else {
+//                 console.log("Fail");
+//             }
+//         });
+//     }
+// }
 
--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,10 +1,10 @@
-import account from "./account";
+import * as ACCOUNT from "./account";
 import nav from "./nav";
 
 const headerContent = document.querySelector("#site-header");
 
 export default function header() {
-    headerContent.appendChild(account());
+    headerContent.appendChild(ACCOUNT.createAccountDiv());
     headerContent.appendChild(nav());
     headerContent.appendChild(document.createElement("hr"));
 }
