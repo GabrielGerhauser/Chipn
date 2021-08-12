de1977720e91e697d7dd82228d8ad6d500043697
jslongwell <84334093+jslongwell@users.noreply.github.com>
2021-08-12 16:04:12 -0400

    Revert "Functional site with login"

--- backend/Chipn/Chipn/Models/Account.cs
+++ backend/Chipn/Chipn/Models/Account.cs
@@ -7,13 +7,15 @@ namespace Chipn.Models
 {
 	public class Account
 	{
-		public int Id { get; set; }
-		public string UserName { get; set; }
-		public string Password { get; set; }
-		public long ChipCount { get; set; }
-		public string Email { get; set; }
-		public int Age { get; set; }
-		public virtual List<AccountGame> AccountGames { get; set; }
+		public int Id {get;set;}
+
+		//
+
+		public string UserName {get;set;}
+		public long ChipCount {get;set;}
+		public string Email {get;set;}
+		public string Password {get;set;}
+		public int Age {get;set;}
 
 		/*
 		public decimal Money {get;set;}
@@ -25,5 +27,9 @@ namespace Chipn.Models
 		public string CreditCard {get;set;}
 		public string SSN {get;set;}
 		*/
+
+		//
+
+
 	}
 }
